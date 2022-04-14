ad244be0941e5f78ed1a255c8f822913b20df752
Andrey <moonhater@yandex.ru>
2022-04-14 19:40:19 +0300

    Первый коммит

---
+++ Task_2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите первое число :");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число :");
+int number_2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число :");
+int number_3 = Convert.ToInt32(Console.ReadLine());
+
+int max = number;
+if (max < number_2) 
+{
+   max = number_2;
+}
+if (max < number_3)
+{
+    max = number_3;
+}
+Console.WriteLine(max);
---
+++ homework/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число :");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число :");
+int number_2 = Convert.ToInt32(Console.ReadLine());
+
+if (number == number_2) 
+{
+    Console.WriteLine("Числа равны");
+}
+else if  (number < number_2) 
+    Console.WriteLine(number_2);
+else 
+    Console.WriteLine(number);
