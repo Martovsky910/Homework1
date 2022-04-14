6aa52fc47c6e386b3d724d1d0134721330d3b7de
Andrey <moonhater@yandex.ru>
2022-04-14 21:42:59 +0300

    homework

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число :");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+    Console.WriteLine("Делится на 2 без остатка");
+}  
+else
+{
+    Console.WriteLine("Не делится на 2 без остатка");
+}
---
+++ Task_4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число :");
+int number = Convert.ToInt32(Console.ReadLine());
+int startnubmer = 1;
+
+while (startnubmer < number)
+{
+   
+    if (startnubmer % 2 == 0)
+    {
+       Console.Write(startnubmer + ". " );
+    }
+    startnubmer++;
+}
